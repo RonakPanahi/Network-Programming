2504a7ff7a4cc27a1134a3f0ee7a4f3d2de0c654
Ronak Panahi <RonakPanahi@users.noreply.github.com>
2016-08-01 17:28:21 -0500

    Update Find_IP_Host.java

--- Find_IP_Host.java
+++ Find_IP_Host.java
@@ -1,3 +1,17 @@
+/**
+ * 
+ * nslookup is a Unix utility that converts hostnames to IP addresses 
+ * and IP addresses to hostnames. It has two modes: interactive and 
+ * command line. If you enter a hostname on the command line, nslookup
+ * prints the IP address of that host. If you enter an IP address on 
+ * the command line, nslookup prints the hostname. If no hostname or 
+ * IP address is entered  on the command line, nslookup enters interactive
+ * mode, in which it reads hostnames and IP addresses from input and echoes
+ * back the corresponding IP addresses or hostnames until you type “exit”.
+ */
+
+
+
 package ip_hostname;
 
 import java.io.DataInputStream;
